ed903d7f6cfa7e9f5ea24eb7490571992291c6ca
DarkKnight <94906343+l0v3m0n3y@users.noreply.github.com>
2024-07-24 01:13:05 +0300

    Update altmails.js

--- altmails.js
+++ altmails.js
@@ -2,7 +2,6 @@ const crypto = require('node:crypto');
 class altmails{
     constructor(){
         this.api = "https://tempmail.altmails.com"
-        this.mailbox=null
         this.headers={"Host":"tempmail.altmails.com","Origin":"https://tempmail.altmails.com","Referer":"https://tempmail.altmails.com/","user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0","x-requested-with": "XMLHttpRequest","Connection": "Keep-Alive"}
     }
     async get_messages(){
